10bf5b199b5caba828279e9926ba32ab82c9b204
reesekin <reesekin@yahoo.com>
2019-12-19 02:12:05 -0700

    Initial Commit

---
+++ coach.js
@@ -0,0 +1,136 @@
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('boomers');
+var list = '';
+var Boomers = [];
+
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS boomer (boomer_tag TEXT, id TEXT)");
+  console.log('\nCoach bot by: Reesekin\n')
+  console.log('Boomer list loaded:\n' + '---------------------')
+  db.all('SELECT tag FROM boomer', function(err, rows)
+          {
+            rows.forEach(function(row){
+              console.log(row.tag);
+              list += ", " + row.tag.toString();
+            })
+          })
+  
+          db.all('SELECT id FROM boomer', function(err, rows)
+          {
+            rows.forEach(function(row){
+              Boomers.push(row.id);
+            })
+          })
+      //stmt.run("Reesekin");
+      //stmt.finalize();
+
+function reload(){
+  db.all('SELECT tag FROM boomer', function(err, rows)
+          {
+            rows.forEach(function(row){
+              list += ", " + row.tag.toString();
+            })
+          });
+}
+
+const token = 'NjU3MDA0ODU1OTU2NjAyOTMw.Xfq7dg.eG7qBF5-R1SrPKOwVsddiVUoEhg';
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+var isReady = true;
+var opus = require('opusscript');
+var samplingRate = 48000;
+var frameDuration = 20;
+var channels = 2;
+const PREFIX = '$';
+
+bot.on('message', message  => {
+
+  if (message.content === 'ultralite') {
+    message.reply('boomer spotted');
+  }
+
+  let args = message.content.substring(PREFIX.length).split(" ")
+
+  let fMent = message.mentions.users.first()
+  var stmt = db.prepare("INSERT INTO boomer VALUES (?, ?)");
+  if (args[1] == undefined && args[0] != 'boomer-list') {}
+    else {
+    switch(args[0]){
+      case 'boomerify':
+            stmt.run(fMent.username, fMent.id);
+            stmt.finalize();
+            message.reply('Boomer ' +  fMent.username + ' is added to the database of boomers!');
+        break;
+      
+      case 'boomer-list':
+        reload();
+        message.reply('List of Boomers' + list);
+        break;
+      case 'boomer-remove':
+        db.run("DELETE FROM boomer WHERE id = " + fMent.id);
+        message.reply( fMent.username + ' has been removed from the database of boomers.')
+        break;
+      case 'boomer-id':
+        message.reply('ID is: ' + message.mentions.users.first().id);
+    }
+}
+  if (isReady && message.content === 'Boomer')
+  {
+  isReady = false;
+  var x = Math.floor((Math.random() * 4) + 1);
+  console.log('Audio file:' + x);
+  var voiceChannel = message.member.voiceChannel;
+  voiceChannel.join().then(connection =>
+  {
+     const dispatcher = connection.playFile('./sounds/boomer' + x + '.mp3');
+     dispatcher.on("end", end => {
+        voiceChannel.leave();
+       });
+   }).catch(err => console.log(err));
+   isReady = true;
+  }
+});
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  let newUserChannel = newMember.voiceChannel
+  let oldUserChannel = oldMember.voiceChannel
+
+
+  if(oldUserChannel === undefined && newUserChannel !== undefined) {
+
+     // User Joins a voice channel
+     db.all('SELECT id FROM boomer', function(err, rows)
+     {
+       rows.forEach(function(row){
+         if(oldMember.id === row.id){
+           console.log('\nBoomer found! : ' + newMember.user.username);
+          if (isReady)
+            {
+            isReady = false;
+            var x = Math.floor((Math.random() * 4) + 1);
+            console.log('Audio file:' + x);
+            var voiceChannel = bot.channels.get(newMember.voiceChannelID);
+            voiceChannel.join().then(connection =>
+            {
+             const dispatcher = connection.playFile('./sounds/boomer' + x + '.mp3');
+             dispatcher.on("end", end => {
+                voiceChannel.leave();
+               });
+           }).catch(err => console.log(err));
+           isReady = true;
+          }
+         }
+       })
+     })
+ 
+
+  } else if(newUserChannel === undefined){
+
+    // User leaves a voice channel
+    
+  }
+})
+
+bot.login(token);
+});
